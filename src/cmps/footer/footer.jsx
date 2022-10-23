5fa2eed8e2c7b85225c15b30c9730142db3b1f85
CarmelYona <carmelyona26@gmail.com>
2022-10-23 12:03:30 +0300

    footer cmp[ done!

--- src/cmps/footer/footer.jsx
+++ src/cmps/footer/footer.jsx
@@ -1,3 +1,26 @@
+import { FaFacebookF } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
+
 export const Footer = () => {
-    return <section>Footer</section>
+    return <footer>
+        <a href="#" className="footer-logo">CARMEL</a>
+        <ul className="permalinks clean-list flex">
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#testimonials">Testimonials</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div className="footer-socials flex">
+            <a href="https://www.facebook.com/carmel.yona/"><FaFacebookF /></a>
+            <a href="https://www.instagram.com/carmelyona/"><FiInstagram /></a>
+        </div>
+
+        <div className="footer-copyright">
+            <small>&copy; Carmel Yona. All rights reserved.</small>
+        </div>
+    </footer>
 }
